90861097d1e524e97137a8dfc43a0ee06269db41
septoctobre <mecaniqueorange@gmail.com>
2016-11-10 17:48:07 +0100

    Surcharge _getSelectCountSql() pour renvoyer le nombre d'enregistrements regroupés

---
+++ app/code/community/DigitalPianism/Abandonedcarts/Model/Resource/Product/Collection.php
@@ -0,0 +1,28 @@
+<?php
+
+class DigitalPianism_Abandonedcarts_Model_Resource_Product_Collection extends Mage_Catalog_Model_Resource_Product_Collection
+{
+    
+    protected function _getSelectCountSql($select = null, $resetLeftJoins = true)
+    {
+        $this->_renderFilters();
+
+        $countSelect = clone $this->getSelect();
+        $countSelect->reset(Zend_Db_Select::ORDER);
+        $countSelect->reset(Zend_Db_Select::LIMIT_COUNT);
+        $countSelect->reset(Zend_Db_Select::LIMIT_OFFSET);
+        $countSelect->reset(Zend_Db_Select::COLUMNS);
+
+        if(count($this->getSelect()->getPart(Zend_Db_Select::GROUP)) > 0) {
+            $countSelect->reset(Zend_Db_Select::GROUP);
+            $countSelect->distinct(true);
+            $group = $this->getSelect()->getPart(Zend_Db_Select::GROUP);
+            $countSelect->columns("COUNT(DISTINCT ".implode(", ", $group).")");
+        } else {
+            $countSelect->columns('COUNT(*)');
+        }
+
+        return $countSelect;
+    }
+    
+}
