ff8edde6a33987883fde08d34a4afb4c6cb55b98
Alberto Tedeschi <atedeschi@webgriffe.com>
2018-10-22 15:49:04 +0200

    Now on the collection will load only quotes updated after delay-24h

--- app/code/community/DigitalPianism/Abandonedcarts/Model/Collection.php
+++ app/code/community/DigitalPianism/Abandonedcarts/Model/Collection.php
@@ -23,8 +23,10 @@ class DigitalPianism_Abandonedcarts_Model_Collection {
         $nameId = $eavAttribute->getIdByCode('catalog_product', 'name');
         $priceId = $eavAttribute->getIdByCode('catalog_product', 'price');
 
+        $minDelay = $delay-86400;
+
         // Normal join condition
-        $emailJoin = sprintf('quote_items.quote_id = quote_table.entity_id AND quote_table.items_count > 0 AND quote_table.is_active = 1 AND quote_table.customer_email IS NOT NULL AND quote_table.abandoned_notified = 0 AND quote_table.updated_at < "%s" AND quote_table.store_id = %s', $delay, $storeId);
+        $emailJoin = sprintf('quote_items.quote_id = quote_table.entity_id AND quote_table.items_count > 0 AND quote_table.is_active = 1 AND quote_table.customer_email IS NOT NULL AND quote_table.abandoned_notified = 0 AND quote_table.updated_at < "%s" AND quote_table.updated_at > "%s" AND quote_table.store_id = %s', $delay, $minDelay, $storeId);
 
         // In case an array of emails has been specified
         if (!empty($emails)) {
